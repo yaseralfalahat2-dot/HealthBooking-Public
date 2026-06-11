6fd0480c916c2e1dec62d55d307796952217dbf3
yaseralfalahat2-dot <yaseralfalahat2@gmail.com>
2026-06-11 11:58:10 +0300

    Update AppointmentsList.vue

--- views/AppointmentsList.vue
+++ views/AppointmentsList.vue
@@ -59,7 +59,7 @@ export default {
   },
   methods: {
     fetchAppointments() {
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments")
+      fetch("https://u82hg3kqak.execute-api.us-east-1.amazonaws.com/prod/appointments")
         .then(res => res.json())
         .then(data => {
           const parsed = JSON.parse(data.body);
@@ -74,7 +74,7 @@ export default {
       console.log("appointmentId:", cleanAppointment.appointmentId);
       console.log(" appointmentId (direct):", appointment.appointmentId);
 
-      const url = `https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments/${appointment.appointmentId}`;
+      const url = `https://u82hg3kqak.execute-api.us-east-1.amazonaws.com/prod/appointments/${appointment.appointmentId}`;
 
       const payload = { status: newStatus };
 
